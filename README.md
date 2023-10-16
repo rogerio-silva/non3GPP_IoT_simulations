f6aeddd2191681a219341e1cbfe2f626a3a35351
rogerio-silva <rogeriosousaesilva@gmail.com>
2023-10-16 15:37:42 -0300

    Update Readme.md

--- README.md
+++ README.md
@@ -1,7 +1,134 @@
-# Source code repository
+# Dynamic resources allocation in non-3GPP IoT networks involving UAVs
 
-## Data files
 
-## Optimization model
+<p align='center' style="margin-bottom: -4px">Rogério S. Silva$$^\ast^\dagger$$, William P. Junior$$^\ast$$, Sand L. Correa$$^\ast$$, Kleber V. Cardoso$$^\ast$$, Antonio Oliveira-JR$$^\ast$$</p>
+<p align='center' style="margin-bottom: -4px">$$^\dagger$$NumbERS, Instituto Federal de Goiás-Inhumas-GO</p>
+<p align='center' style="margin-bottom: -4px">E-mail: {rogerio.sousa}@ifg.edu.br</p>
+<p align='center' style="margin-bottom: -4px">$$^\ast$$Instituto de Informática, Universidade Federal de Goiás</p>
+<p align='center'>E-mail: {williamtpjunior, sand, kleber, antonio}@inf.ufg.br</p>
 
-## Simulator model
+## Abstract
+
+In this work, we investigate how to minimize the number of gateways deployed in Unmanned Aerial Vehicles (UAVs) needed to meet the demand for non-3GPP Internet of Things (IoT) devices, seeking to improve the Quality of Service (QoS), keeping a balance between delay and data rate. Gateways deployed in UAVs add the mobility flexibility of UAVs, which paves the way for meeting emergency demand increments. The 5th Generation Networks (5G) and Beyond 5th Generation Networks (B5G) systems incorporated access to IoT devices via non-3GPP access, opening up new integration  possibilities. Furthermore, Low Power Wide Area Network (LPWAN) networks, especially Long Range Wide Area Network (LoRaWAN), allow access over long distances with reduced energy consumption. In this scenario, our work proposes a Mixed Integer Linear Programming (MILP) optimization model to minimize the number of UAVs that meet the increment of emergency demand, comply with limits of QoS, and maintain the compromise between delay and data rate. Simulation results show that the proposed model significantly reduces the number of UAVs, maintains optimal levels of QoS, and maintains the compromise between delay and data rate compared to the baselines.
+
+## Baselines
+Two methods are defined as baselines for our proposal, the Density-oriented (DO) and the Equidistant (EQ) UAVs placement models. 
+### {Equidistant UAVs placement algorithm (EQ)}
+EQ was developed to distribute the UAVs across the area and maintain the equidistance between them. The input data contains the placement area $\mathcal{A}_{(MxN)}$ and the lower bound QoS  to be reached. The algorithm iterates by incrementing the number of UAVs to find how many satisfy the $QoS_{bound}$. The inter-UAV distance is computed for each UAV added, and the UAVs are distributed, ensuring equidistance. This algorithm places the UAVs in the perspective of the coverage area and does not consider the positions of the EDs.
+### {Density-oriented UAVs placement algorithm (DO)}
+DO was developed to place UAVs in the most populated areas, following the EDs density map. The input data contains the map with the coordinates of EDs $\beta_{(MxN)}$. Initially, the area density information is calculated, then $\beta_{(MxN)}$ is linearized, sorted in descending order, and assigned to $\rho_{(L)}$. The algorithm iterates until reaching the target bound $QoS_{bound}$. In each iteration, a new UAV is added and placed in the corresponding position $\rho{(l)}$ of matrix of $\alpha_{(m,n)}$, where $l$ represents the linear index that corresponds to a $(m,n)$. The DO outputs a UAVs placement map $\alpha_{(m,n)}$.
+
+## Proposed placement algorithm
+
+<img src="/images/OP_Algorithm.png" width="500">
+
+The OP algorithm presents our approach for positioning UAVs-based gateways following the optimization model in section 2. The algorithm receives as input the area for positioning devices ($\mathcal{A}_{(MxN)}$), the UAV placement area ($\mathcal{V}_{(XxYxZ)}$) and the expected QoS limit ($\rho^{QoS}_l$). In line 1, the devices are distributed following a realistic spatial distribution (here). In lines 2 and 3, the set of communication parameters between the devices and each virtual position, in addition to the association of the devices to the respective slices. In line 4, the optimization algorithm is executed. The OP results in the UAVs positions set $\mathcal{P}$, the SF (spreading factor) and TP (transmission power) configurations set $\mathcal{C}$, and the slices associations set $\mathcal{S}$. Finally, the resulting data are modeled in the NS-3, and the simulation is performed.
+
+## Results
+The results were obtained from 50 executions for 40, 50, and 60 devices. In each execution, a group of devices and then the UAVs are deployed according to the algorithms EQ, DO, and OP, presented earlier. 
+
+### UAVs placement
+
+<p align='center'>
+    <img src='/images/UAVs_Placements.png' width='500'>
+</p>    
+<p align='center'>
+    <figurecaption>
+        Fig. 1. UAVs placement.
+    </figurecaption>
+</p>
+
+Fig. 1 shows the area for UAVs placement in a scenario with 60 EDs. Colored symbols represent UAVs for each placement method, and black circles represent devices.
+
+### Number of uavs needed to serve the devices ensuring the QoS level. 
+<p align='center'> <img src="/images/UAVsXDevices.png" width="500"> </p>
+<p align='center'>
+    <figurecaption>
+        Fig. 2. Devices serviced and number of UAVs required.
+    </figurecaption>
+</p>
+
+Figure 2 presents the number of UAVs needed to meet the QoS threshold determined as a target. For a Qbound = 0.9, OP presents the best results, e.g., to meet an average above 50 EDs, with a standard deviation near to the other algorithms, four UAVs were deployed with OP, while DO and EQ needs more. The DO and EQ have a less pronounced growth curve for serving the EDs, which impacts a higher number of deployed UAVs. Another critical point to evaluate is the number of effective runs to obtain each point. In a universe of 50 runs, for each set of EDs, we consider effective those that resulted in a Qbound ≥ 0.9; the highlights of the numerical annotations in the graph denote these effective rounds.
+
+### QoS
+<p align='center'> <img src="/images/Heatmap_QoS.png" width="700"></p>
+<p align='center'>
+    <figurecaption>
+        Fig. 3. QoS.
+    </figurecaption>
+</p>
+
+Figure 3 displays results relative to QoS. The large areas in light colors for OP demonstrate the excellent efficiency of the proposed algorithm. Furthermore, it is possible to observe that the number of unanswered EDs is more minor than DO and EQ, denoted by the black dots. Another highlight about DO and EQ is that in addition to presenting the highest rates of EDs not met, the rates of QoS reached are much worse than in OP, that is denoted by the red, and purple dots on the respective figures.
+
+# Replicating The Experiment
+
+## Requirements
+
+- GNU (>=8.0.0)
+- CMAKE (>=3.24)
+- python (3.11.4)
+- [SCIP Optimization Suite (8.0.3)](https://scipopt.org/index.php#download)
+
+## Preparing Environment
+
+Start by cloning this repository.
+
+```bash
+git clone https://github.com/LABORA-INF-UFG/non3GPP_IoT_simulations.git 
+cd non3GPP_IoT_simulations
+```
+
+The first step is to build the version 3.36 of NS3.
+
+```bash
+git clone https://github.com/nsnam/ns-3-dev-git ns-3
+cd ns-3
+git checkout ns-3.36
+cp -r ../contrib/* ./contrib/
+ln -s ~/non3GPP_IoT_simulations/scratch/optimal-distrib-experiment.cc ~/non3GPP_IoT_simulations/ns-3/scratch/optimal-distrib-experiment.cc
+ln -s ~/non3GPP_IoT_simulations/scratch/thesis-experiment.cc ~/non3GPP_IoT_simulations/ns-3/scratch/thesis-experiment.cc
+
+./ns3 configure --enable-examples
+./ns3 build
+```
+
+The following python packages are needed to execute the experiment.
+
+```bash
+pip install pyomo pandas matplotlib blob
+```
+
+We can then start the experimentation process, after every step you can check the generated files inside [data/](data/) folder.
+
+### 1st Step - Generating Input Data
+
+use the notebook [`equidistantPlacement.ipynb`](./equidistantPlacement.ipynb) to generate the files with the virtual positions for LoRa-GWs.
+
+Generate the files with LoRa-ED positions using the NS3 script, you can modify the number of devices with the option `--nDevices=x` and the seed for the pseudo random distribution of the devices with the option `--seed=y`.
+
+```bash
+./ns-3/build/scratch/ns3.36-devices-density-oriented-distrib-default --nDevices=30 --seed=1
+```
+
+To finalize, generate the files with slice association of the devices.
+
+```bash
+./ns-3/build/scratch/ns3.36-thesis-experiments-default --nDevices=30 --seed=1 --nGateways=25 --nPlanes=1 --verbose
+```
+
+### 2nd Step - Optimization Model
+
+To execute the optimization model, we need to pass in order the number of virtual positions (25), number of height levels (1), number of devices (30), device distribution seed (1) and QoS lower bound (0.9). 
+
+```bash
+python model.py 25 1 30 1 0.9
+```
+
+### 3rd Step - Simulation
+
+We are ready to execute the simulation with the solution obtained in the last step. 
+
+```bash
+./ns-3/build/scratch/ns3.36-optimal-distrib-experiment-default --nDevices=30 --seed=1 --nGateways=25 
+
+The simulation output can be found in the directory [./data/results/](./data/results/).
---
+++ scratch/devices-density-oriented-distrib.cc
@@ -0,0 +1,296 @@
+#include "ns3/end-device-lora-phy.h"
+#include "ns3/simulator.h"
+#include "ns3/log.h"
+#include "ns3/constant-position-mobility-model.h"
+#include "ns3/mobility-helper.h"
+#include "ns3/node-container.h"
+#include "ns3/position-allocator.h"
+#include "ns3/command-line.h"
+#include "ns3/core-module.h"
+#include "ns3/lorawan-module.h"
+#include "ns3/propagation-module.h"
+#include <algorithm>
+#include <iomanip>
+#include <unistd.h>
+
+using namespace ns3;
+using namespace lorawan;
+
+NS_LOG_COMPONENT_DEFINE ("DevicesDensityOrientedPlacement");
+
+struct pos
+{
+  double v;
+  int w;
+  int h;
+};
+
+bool
+pos_compare (const pos &p1, const pos &p2)
+{
+  return p1.v > p2.v;
+}
+
+int
+main (int argc, char *argv[])
+{
+  int nDevices = 0;
+  int seed = 1;
+  bool printTrafficDensity = false;
+
+  CommandLine cmd;
+  cmd.AddValue ("nDevices", "Number of end devices to include in the simulation", nDevices);
+  cmd.AddValue ("seed", "Independent replications seed", seed);
+  cmd.AddValue ("printTrafficDensity",
+                "Whether to print traffic density values to graphic validation ",
+                printTrafficDensity);
+  cmd.Parse (argc, argv);
+
+  ns3::RngSeedManager::SetSeed (seed);
+
+  //  LogComponentEnable ("DevicesDensityOrientedPlacement", LOG_LEVEL_ALL);
+  //  LogComponentEnableAll (LOG_PREFIX_FUNC);
+  //  LogComponentEnableAll (LOG_PREFIX_NODE);
+  //  LogComponentEnableAll (LOG_PREFIX_TIME);
+  //  LogComponentEnableAll (LOG_ERROR);
+
+  NS_LOG_INFO ("Creating end devices...");
+  // Calculate density demand map
+  // Generate the demand according to the following paper:
+  // Dongheon Lee; Sheng Zhou; Xiaofeng Zhong; Zhisheng Niu; Xuan Zhou;
+  // Honggang Zhang, "Spatial modeling of the traffic density in cellular
+  // networks," in Wireless Communications, IEEE , vol.21, no.1, pp.80-88,
+  // February 2014. doi: 10.1109/MWC.2014.6757900
+
+  //  Some parameters
+  //      %mu = 25.7015; % scaled
+  //      mu = 18.93; % (DL-urban)
+  //      %mu=17.7956; %(UL-urban), mu=12.572 (DL-rural), mu=11.573 (UL-rural)
+  //      %sigma = 1.1929; % scaled
+  //      %sigma = 2.3991; % (DL-urban)
+  //      sigma=2.1188;% (UL-urban);%, sigma=2.7985 (DL-rural), sigma=2.3055 (UL-rural)
+  //      %wMax = 0.001163; % test
+  //      wMax = 0.011592; % (DL-urban)
+  //      %wMax=0.012673;% (UL-urban);%, wMax=0.001163 (DL-rural), wMax=0.001202 (UL-rural)
+
+  struct densityParams
+  {
+    double area_width = 10000; // Area width in meters
+    double area_height = 10000; // Area height in meters
+    double pixSideLen = 2000; // Cell height and width
+    double mu = 17.7956;
+    double sigma = 2.1188;
+    double w_max = 0.012673;
+  };
+
+  densityParams dParams;
+  double a, b;
+  double angFreq_i[10], angFreq_j[10], phase_phi[10], phase_psi[10];
+
+  Ptr<UniformRandomVariable> rd = CreateObject<UniformRandomVariable> ();
+  rd->SetAttribute ("Min", DoubleValue (0.0));
+  rd->SetAttribute ("Max", DoubleValue (1.0));
+
+  for (int i = 0; i < 10; ++i)
+    {
+      a = 0;
+      b = dParams.w_max;
+      angFreq_i[i] = a + (b - a) * rd->GetValue ();
+      angFreq_j[i] = a + (b - a) * rd->GetValue ();
+      a = 0;
+      b = 2 * M_PI;
+      phase_phi[i] = a + (b - a) * rd->GetValue ();
+      phase_psi[i] = a + (b - a) * rd->GetValue ();
+    }
+
+  // Compute cell density
+  int ppW = floor (dParams.area_width / dParams.pixSideLen);
+  int ppH = floor (dParams.area_height / dParams.pixSideLen);
+  double Xwh, Ywh, cum_pGwh, totDens = 0.0;
+  double pGwh[ppW][ppH];
+  double p_wh[ppW][ppH];
+  for (int w = 0; w < ppW; w++)
+    {
+      for (int h = 0; h < ppH; h++)
+        {
+          Xwh = w * dParams.pixSideLen - 0.5 * dParams.pixSideLen;
+          Ywh = h * dParams.pixSideLen - 0.5 * dParams.pixSideLen;
+          cum_pGwh = 0;
+          for (int i = 0; i < 10; i++)
+            {
+              cum_pGwh = cum_pGwh + cos (angFreq_i[i] * Xwh + phase_phi[i]) *
+                                        cos (angFreq_j[i] * Ywh + phase_psi[i]);
+            }
+          pGwh[w][h] = (2 / sqrt (10)) * cum_pGwh;
+          p_wh[w][h] = exp (dParams.sigma * pGwh[w][h] + dParams.mu);
+          totDens = totDens + p_wh[w][h];
+        }
+    }
+
+  // Normalize cell values to nDevices
+  double pN_wh[ppW][ppH]; // Percentage of devices per cell
+  double nDev[ppW][ppH]; // Number of devices per cell
+  std::vector<pos> vecPos;
+  for (int w = 0; w < ppW; w++)
+    {
+      for (int h = 0; h < ppH; h++)
+        {
+          pN_wh[w][h] = (p_wh[w][h]) / totDens;
+          nDev[w][h] = (pN_wh[w][h] * nDevices);
+          vecPos.push_back ({nDev[w][h], w, h});
+        }
+    }
+
+  // order the densest first
+  std::sort (vecPos.begin (), vecPos.end (), pos_compare);
+  int lastPos, nElements;
+  for (lastPos = 0, nElements = 0; vecPos[lastPos].v != 0; lastPos++, nElements+=((int)vecPos[lastPos].v))
+    ;
+
+  // Put the devices in the respective cells
+  NodeContainer tmpDevices[lastPos];
+  NodeContainer devicesContainer;
+  MobilityHelper tmpMobility[lastPos];
+  int posFactor = (int) dParams.area_height / ppH;
+  int devPosit = 0, devToPos;
+  for (int x = 0; x < lastPos && nDevices-devPosit > 0; x++)
+    {
+      devToPos = round(vecPos[x].v)>0?(int) round(vecPos[x].v):1;
+      tmpDevices[x].Create (devToPos);
+      devPosit+=devToPos;
+      tmpMobility[x].SetPositionAllocator (
+          "ns3::RandomRectanglePositionAllocator", "X",
+          PointerValue (CreateObjectWithAttributes<UniformRandomVariable> (
+              "Min", DoubleValue (vecPos[x].w * posFactor), "Max",
+              DoubleValue (vecPos[x].w * posFactor + posFactor))),
+          "Y",
+          PointerValue (CreateObjectWithAttributes<UniformRandomVariable> (
+              "Min", DoubleValue (vecPos[x].h * posFactor), "Max",
+              DoubleValue (vecPos[x].h * posFactor + posFactor))));
+      tmpMobility[x].SetMobilityModel ("ns3::ConstantPositionMobilityModel");
+      tmpMobility[x].Install (tmpDevices[x]);
+      devicesContainer.Add (tmpDevices[x]);
+    }
+  //  //Put the devices in the respective cells
+  //  NodeContainer tmpDevices[ppW][ppH];
+  //  NodeContainer devicesContainer;
+  //  MobilityHelper tmpMobility[ppW][ppH];
+  //  int posFactor = dParams.area_height/ppH;
+  //  for (int w = 0; w < ppW; w++)
+  //    {
+  //      for (int h = 0; h < ppH; h++)
+  //        {
+  //          if (nDev[w][h] > 0)
+  //            {
+  //              tmpDevices[w][h].Create (nDev[w][h]);
+  //              tmpMobility[w][h].SetPositionAllocator (
+  //                  "ns3::RandomRectanglePositionAllocator", "X",
+  //                  PointerValue (CreateObjectWithAttributes<UniformRandomVariable> (
+  //                      "Min", DoubleValue (w * posFactor), "Max", DoubleValue (w * posFactor + posFactor))),
+  //                  "Y",
+  //                  PointerValue (CreateObjectWithAttributes<UniformRandomVariable> (
+  //                      "Min", DoubleValue (h * posFactor), "Max", DoubleValue (h * posFactor + posFactor))));
+  //              tmpMobility[w][h].SetMobilityModel ("ns3::ConstantPositionMobilityModel");
+  //              tmpMobility[w][h].Install (tmpDevices[w][h]);
+  //              devicesContainer.Add (tmpDevices[w][h]);
+  //            }
+  //        }
+  //    }
+
+  //  Unallocated devices
+
+//  MobilityHelper tmpMob;
+//  NodeContainer tmpDev;
+//  if (devicesContainer.GetN () < (u_int32_t) nDevices)
+//    {
+//      tmpDev.Create (nDevices - devicesContainer.GetN ());
+//      tmpMob.SetPositionAllocator ("ns3::RandomRectanglePositionAllocator", "X",
+//                                   PointerValue (CreateObjectWithAttributes<UniformRandomVariable> (
+//                                       "Min", DoubleValue (0), "Max", DoubleValue (10000))),
+//                                   "Y",
+//                                   PointerValue (CreateObjectWithAttributes<UniformRandomVariable> (
+//                                       "Min", DoubleValue (0), "Max", DoubleValue (10000))));
+//      tmpMob.SetMobilityModel ("ns3::ConstantPositionMobilityModel");
+//      tmpMob.Install (tmpDev);
+//      devicesContainer.Add (tmpDev);
+//    }
+
+  Ptr<UniformRandomVariable> rz = CreateObject<UniformRandomVariable> ();
+  rz->SetAttribute ("Min", DoubleValue (1.0));
+  rz->SetAttribute ("Max", DoubleValue (3.0));
+  for (auto j = devicesContainer.Begin (); j != devicesContainer.End (); ++j)
+    {
+      Ptr<MobilityModel> mobility2 = (*j)->GetObject<MobilityModel> ();
+      Vector position = mobility2->GetPosition ();
+      position.z = rz->GetValue ();
+      mobility2->SetPosition (position);
+    }
+
+  // Print positions into a file
+  std::string cwd = get_current_dir_name();
+  std::string path = cwd + "/data/placement/";
+  if (printTrafficDensity)
+    {
+      std::string fileTrafficDensity = path + "trafficDensity_" + std::to_string (seed) + ".dat";
+      std::string fileTrafficDensityNormalized =
+          path + "trafficNormalized_" + std::to_string (seed) + ".dat";
+
+      const char *cT = fileTrafficDensity.c_str ();
+      const char *cN = fileTrafficDensityNormalized.c_str ();
+
+      std::ofstream trafficFile;
+      trafficFile.open (cT);
+      std::ofstream normalizedFile;
+      normalizedFile.open (cN);
+
+      int i;
+      for (i = 0; i < ppH - 1; i++)
+        {
+          trafficFile << i << " ";
+          normalizedFile << i << " ";
+        }
+      trafficFile << i << "\n";
+      normalizedFile << i << "\n";
+
+      int j;
+      for (i = 0; i < ppH; i++)
+        {
+          for (j = 0; j < ppW - 1; j++)
+            {
+              trafficFile << p_wh[i][j] << " ";
+              normalizedFile << pGwh[i][j] << " ";
+            }
+          trafficFile << p_wh[i][j] << "\n";
+          normalizedFile << pGwh[i][j] << "\n";
+        }
+      trafficFile.close ();
+      normalizedFile.close ();
+    }
+
+  std::string fileNS3 = path + "endDevices_LNM_Placement_" + std::to_string (seed) + "s+" +
+                        std::to_string (nDevices) + "d.dat";
+  const char *cN = fileNS3.c_str ();
+
+  std::ofstream devicesNS3File;
+  devicesNS3File.open (cN);
+
+  for (auto dv = devicesContainer.Begin (); dv != devicesContainer.End (); ++dv)
+    {
+      Ptr<MobilityModel> mobility2 = (*dv)->GetObject<MobilityModel> ();
+      Vector position = mobility2->GetPosition ();
+      devicesNS3File << position.x << " " << position.y << " " << position.z << std::endl;
+    }
+  devicesNS3File.close ();
+
+  /****************
+  *  Simulation  *
+  ****************/
+
+  Simulator::Stop ();
+
+  Simulator::Run ();
+
+  Simulator::Destroy ();
+
+  return 0;
+}
